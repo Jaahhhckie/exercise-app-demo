e5ac2697b8190043339ea826ef6ca4faf5141a56
Jackie <Ardiles.Jackeline@hotmail.com>
2024-03-04 23:25:58 -0500

    exercise-app-demo

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import RepetitionExercise from './components/RepetitionExercise';
+import Durationexercise from './components/DurationExercise';
+import DistanceExercise from './components/DistanceExercise';
+
+function App() {
+  const [selectedExercise, setSelectedExercise] = useState(null);
+
+  const handleExerciseClick = (exerciseType) => {
+    setSelectedExercise(exerciseType);
+  };
+
+  return (
+    <div className="App">
+      <h1>Exercise Tracker</h1>
+      <button onClick={() => handleExerciseClick('repetition')}>Push-ups</button>
+      <br></br>
+      <button onClick={() => handleExerciseClick('duration')}>Running</button>
+      <br></br>
+      <button onClick={() => handleExerciseClick('distance')}>Walking</button>
+
+      {selectedExercise === 'repetition' && <RepetitionExercise name="Push-ups" />}
+      {selectedExercise === 'duration' && <Durationexercise name="Running" />}
+      {selectedExercise === 'distance' && <DistanceExercise name="Walking" />}
+    </div>
+  );
+}
+
+
+export default App;
